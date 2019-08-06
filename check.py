3e6042f1ac4dccae920556dfb08338c5c90a5b8f
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-08-06 19:05:29 +0000

    first commit

---
+++ check.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def run_command(cmd, shell=False, strip_newline=True):
+    "run a command and return (stdout, stderr, exit code)"
+
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=shell
+    )
+
+    stdout, stderr = process.communicate()
+
+    stdout = stdout.decode()
+    stderr = stderr.decode()
+
+    if strip_newline:
+        stdout = stdout.rstrip("\n")
+        stderr = stderr.rstrip("\n")
+
+    return stdout, stderr, process.returncode
+
+stdout, stderr, return_code = run_command(["ps", "-p", "4398", "-o", "%cpu,%mem", "--no-headers"])
+
+cpu, _, mem = stdout.split(" ")
+
+cpu = float(cpu)
+mem = float(mem)
+
+print(cpu, mem)
+
+
